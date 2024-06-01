a5083a34164b051be8a3100c5249568233b733ea
Anurag Yadav <anuragyadav20062002@gmail.com>
2024-06-01 17:38:56 +0530

    added shadcn and tailwind css

--- my-ocr-app/vite.config.js
+++ my-ocr-app/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import shadcnui from '@shadcn/ui/plugin'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), shadcnui()],
 })
